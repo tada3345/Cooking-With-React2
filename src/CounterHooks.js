3e4a944e44a0b0d247cef67a46ce2f50cb287d04
tada3345 <tada3345@gmail.com>
2022-08-14 01:27:50 +0900

    first commit

---
+++ src/CounterHooks.js
@@ -0,0 +1,14 @@
+import React, {useState, useContext} from 'react'
+import {ThemeContext} from './App'
+export default function CounterHooks({initialCount}){
+    console.log('Render Counter hooks!')
+    const [count,setCount] =useState(initialCount);
+    const style = useContext(ThemeContext);
+    return (
+        <div>
+        <button style={style} onClick={()=>setCount(prevCount=>prevCount-1)}>-1</button>
+        <span>{count}</span>
+        <button style={style} onClick={()=>setCount(prevCount=>prevCount+1)}>+1</button>
+        </div>
+    )
+}
